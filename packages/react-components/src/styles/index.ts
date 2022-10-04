e394b8b4b65929950a8f22b9d5c439b0ca703de5
Carlos Zabala <cazabala16@gmail.com>
2022-10-05 00:50:23 +0200

    add sortium logo and change default color for local node

--- packages/react-components/src/styles/index.ts
+++ packages/react-components/src/styles/index.ts
@@ -21,10 +21,10 @@ const FACTORS = [0.2126, 0.7152, 0.0722];
 const PARTS = [0, 2, 4];
 const VERY_DARK = 16;
 
-const defaultHighlight = '#D90C51'; // '#D90C51'; // #999
+const defaultHighlight = '#457881'; // '#457881'; // #999
 
 function getHighlight (uiHighlight: string | undefined): string {
-  return (uiHighlight || defaultHighlight);
+  return (defaultHighlight || uiHighlight); // TODO: uiHighlight should be first
 }
 
 function countBrightness (uiHighlight: string | undefined): number {
